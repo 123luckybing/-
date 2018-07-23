172917064c9a5087fa6d2b728d7d3d73d69dae8b
“zhoufangbing” <1978467950@qq.com>
2018-07-24 00:01:06 +0800

    demo

--- reactDemo/src/component/StarScore.js
+++ reactDemo/src/component/StarScore.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import '../css/StarScore.css';
 class StarScore extends Component {
+  constructor() {
+    super();
+    this.state = {
+      num: 2,
+    }
+    this.click = this.click.bind(this);
+  }
+  click(e) {
+    this.setState({
+      num: e.target.dataset.index,
+    });
+  }
   render() {
+    let arr = [1,2,3,4,5];
+    let starList = arr.map((elem,index) => {
+      return(
+        <li
+        onClick={this.click}
+        className={ Number(`${index}`) < Number(`${this.state.num}`)? 'selected' : 'item'}
+        key={index}
+        data-index={elem}
+        // data- 自定义属性，取的话dataset.
+        >
+        </li>
+      )
+    });
     return (
       <div>
-        <Link to='/'><button>返回首页</button></Link>
+        <Link to='/'><button>返回首页</button></Link><br/>
+        <ul className="wrapper">
+          {starList}
+        </ul>
       </div>
     );
   }
